f61a370ddba271b4e233560ae849010bf3ecdcf7
Iris McDonald <S00507769@acad.tri-c.edu>
2018-03-21 14:01:06 -0400

    Add project files.

---
+++ online.database/DVD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online.database
+{
+    public class DVD
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+        public int RunTime { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Price { get; set; }
+        public string Plot { get; set; }
+        
+    }
+}
